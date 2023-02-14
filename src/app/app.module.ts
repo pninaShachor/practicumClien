6f2f4f251be1add553724ec558e18d4d9b1dd159
pninaShachor <sarasypy@gmail.com>
2023-02-14 15:40:42 +0200

    my commit message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { InstructionsComponent } from './instructions/instructions.component';
+import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
+import { EndComponent } from './end/end.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatRadioModule} from '@angular/material/radio';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InstructionsComponent,
+    FormComponent,
+    EndComponent, 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
